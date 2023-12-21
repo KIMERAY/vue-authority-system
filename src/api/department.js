48d6dc0513102c14e8bdcf5368100948a707c955
kimeray <kimerayforw@outlook.com>
2023-12-22 02:55:14 +0800

    feat: 选择所属部门

--- src/api/department.js
+++ src/api/department.js
@@ -1,8 +1,5 @@
 import http from "@/utils/request";
 
-
-
-
 export default{
   /**
    * 查询部门列表
@@ -11,5 +8,13 @@ export default{
    */
   async getDepartmentList(params){
     return await http.get("/api/department/list",params);
+  },
+
+  /**
+   * 获取所属部门列表
+   * @returns
+   */
+  async getParentTreeList(){
+    return await http.get("/api/department/parent/list");
   }
 }
--- src/views/system/department/department.vue
+++ src/views/system/department/department.vue
@@ -76,7 +76,11 @@
           size="small"
         >
           <el-form-item label="所属部门" prop="parentName">
-            <el-input v-model="dept.parentName"></el-input>
+            <el-input
+              v-model="dept.parentName"
+              :readonly="true"
+              @click.native="openSelectDeptWindow()"
+            ></el-input>
           </el-form-item>
           <el-form-item label="部门名称" prop="departmentName">
             <el-input v-model="dept.departmentName"></el-input>
@@ -93,6 +97,44 @@
         </el-form>
       </div>
     </system-dialog>
+    <!-- 选择所属部门窗口 -->
+    <system-dialog
+      :title="parentDialog.title"
+      :visible="parentDialog.visible"
+      :width="parentDialog.width"
+      :height="parentDialog.height"
+      @onClose="onParentClose()"
+      @onConfirm="onParentConfirm()"
+    >
+      <div slot="content">
+        <el-tree
+          ref="parentTree"
+          :data="treeList"
+          node-key="id"
+          :props="defaultProps"
+          empty-text="暂无数据"
+          :highlight-current="true"
+          :default-expand-all="true"
+          :expand-on-click-node="false"
+          @node-click="handleNodeClike"
+        >
+          <div class="customer-tree-node" slot-scope="{ node, data }">
+            <!-- 判断当前节点的子节点是否为0 -->
+            <span v-if="data.children.length === 0">
+              <i class="el-icon-document"></i>
+            </span>
+            <span v-else @click="changeIcon(data)">
+              <svg-icon
+                v-if="data.open"
+                icon-class="round_add_light"
+              ></svg-icon>
+              <svg-icon v-else icon-class="Sub"></svg-icon>
+            </span>
+            <span style="margin-left: 3px">{{ node.label }}</span>
+          </div>
+        </el-tree>
+      </div>
+    </system-dialog>
   </el-main>
 </template>
 
@@ -102,6 +144,7 @@ import departmentApi from "@/api/department";
 // 导入SystemDialog组件
 import SystemDialog from "@/components/system/SystemDialog.vue";
 import { pid } from "process";
+import { log } from "console";
 
 export default {
   name: "department",
@@ -124,7 +167,6 @@ export default {
         departmentName: "", //部门名称
         pid: "", //所属部门id
         parentName: "", //所属部门名称
-
         phone: "", //电话
         address: "", //地址
         orderNum: "", //序号
@@ -137,6 +179,20 @@ export default {
         departmentName: [
           { required: true, message: "请输入部门名称", trigger: "blur" },
         ],
+      },
+      // 选择所属部门窗口属性
+      parentDialog: {
+        title: "选择所属部门", // 窗口标题
+        visible: false, // 是否显示窗口
+        width: 300, // 窗口宽度
+        height: 400, // 窗口高度
+      },
+      treeList: [], // 树形数据
+
+      // 树形组件默认属性值
+      defaultProps: {
+        children: "children",
+        label: "departmentName",
       },
     };
   },
@@ -179,7 +235,112 @@ export default {
       // 关闭窗口
       this.deptDialog.visible = false;
     },
+    /**
+     * 打开选择所属部门窗口
+     */
+    async openSelectDeptWindow() {
+      // 设置窗口属性
+      this.parentDialog.visible = true;
+      // 查询所属部门列表
+      let res = await departmentApi.getParentTreeList();
+      // 判断是否成功
+      if (res.success) {
+        this.treeList = res.data;
+      }
+    },
+    /**
+     * 点击树节点加减号时触发
+     * @param data
+     */
+    changeIcon(data) {
+      // 修改折叠状态
+      data.open = !data.open;
+      this.$refs.parentTree.store.nodesMap[data.id].expanded = !data.open;
+    },
+    /**
+     * 树节点点击事件
+     * @param data
+     */
+    handleNodeClike(data) {
+      // 将选中的部门复制给dept对象
+      this.dept.id = data.id;
+      this.dept.parentName = data.departmentName;
+    },
+    /**
+     * 关闭所属部门取消事件
+     */
+    onParentClose() {
+      this.parentDialog.visible = false;
+    },
+    /**
+     * 选择所属部门确认事件
+     */
+    onParentConfirm() {
+      this.parentDialog.visible = false;
+    },
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+::v-deep .el-tree {
+  .el-tree-node {
+    position: relative;
+    padding-left: 10px;
+  }
+  .el-tree-node__children {
+    padding-left: 20px;
+  }
+  .el-tree-node :last-child:before {
+    height: 40px;
+  }
+  .el-tree > .el-tree-node:before {
+    border-left: none;
+  }
+  .el-tree > .el-tree-node:after {
+    border-top: none;
+  }
+  .el-tree-node:before,
+  .el-tree-node:after {
+    content: "";
+    left: -4px;
+    position: absolute;
+    right: auto;
+    border-width: 1px;
+  }
+  .tree :first-child .el-tree-node:before {
+    border-left: none;
+  }
+  .el-tree-node:before {
+    border-left: 1px dotted #d9d9d9;
+    bottom: 0px;
+    height: 100%;
+    top: -25px;
+    width: 1px;
+  }
+  .el-tree-node:after {
+    border-top: 1px dotted #d9d9d9;
+    height: 20px;
+    top: 14px;
+    width: 24px;
+  }
+  .el-tree-node__expand-icon.is-leaf {
+    width: 8px;
+  }
+  .el-tree-node__content > .el-tree-node__expand-icon {
+    display: none;
+  }
+  .el-tree-node__content {
+    line-height: 30px;
+    height: 30px;
+    padding-left: 10px !important;
+  }
+}
+::v-deep .el-tree > div {
+  &::before {
+    display: none;
+  }
+  &::after {
+    display: none;
+  }
+}
+</style>
